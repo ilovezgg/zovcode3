1831639d0245f2ff78d51be168594046400c12dc
ilovezgg <bottlecvv@gmail.com>
2025-01-31 19:12:03 +0300

    Описание ваших изменений

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import Main from './components/main/Main';
+import ChoouseUs from './components/chooseus/ChoouseUs';
 function App() {
   return (
     <div className="App">
      <Main/>
+     <ChoouseUs/>
+
     </div>
   );
 }
---
+++ src/components/chooseus/ChoouseUs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import z from './ChoouseUs.module.css'
+const ChoouseUs = () => {
+  return (
+    <div>
+        <div className={z.ChooseUs}>
+
+       
+    <div className={z.picChoose}>
+    
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default ChoouseUs
