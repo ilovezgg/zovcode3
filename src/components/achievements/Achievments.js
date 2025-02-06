e0a9c400839abb46e17cb9531f5f2c0262cdd81a
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-06 18:30:14 +0300

    cool

---
+++ src/components/achievements/Achievments.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import z from './Achievments.module.css';
+const Achievments = () => {
+    
+  return (
+    <div>
+         
+    </div>
+      
+  )
+}
+
+export default Achievments
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,16 @@ const Header = () => {
   return (
     <div>
       <div className={z.mainHeader}>
+        <div className={z.mainHeaderTwo}> 
+        <div className={z.headerButtons}>
+            <button className={z.mainButton}>Главная</button>
+            <button className={z.aboutusButton}>О нас</button>
+            <button className={z.contactsButton}>Контакты</button>
+            <button className={z.catalogButton}>Каталог</button>
+            <button className={z.callbackButton}>Обратный звонок</button>
+        </div>
+        </div>
+       
         <div className={z.headerTitle}>
             Качественные дома из профилированного бруса.
             Мы - синоним слову "Надежность"
