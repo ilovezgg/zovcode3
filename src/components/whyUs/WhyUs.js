b5f314d6a9bf3abf1bb2b9d9d6f0f6fd3c0eeb56
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-13 19:11:49 +0300

    13.02.

--- src/components/whyUs/WhyUs.js
+++ src/components/whyUs/WhyUs.js
@@ -5,15 +5,62 @@ const WhyUs = () => {
     <div>
       <div className={z.whyUsMain}>
         <div className={z.backgroundBig}>
-          <div className={z.whyUsTitle}>
-          Почему мы?
+            <div>
+            <div className={z.whyUsTitle}>
+           Плюсы работы с нами
           </div>
-          <div className={z.containerForAll}>
           <div className={z.whyUsText}>
-           
+           Выбирая нас, вы выбираете удобство, скорость и надежность. Мы создаем те дома, в которых хочется жить, воплощая все ваши мечты об идеальном доме в реальность.
           </div>
+            </div>
+          
+          <div className={z.containerForAll}>
+          
           <div className={z.whyUsPanels}>
+          <div className={z.panel1}>
+           <div className={z.picPanel1}>
 
+           </div>
+           <div className={z.titlePanel1}>
+            Контроль нашей работы на всех этапах
+           </div>
+           <div className={z.textPanel1}>
+           Надежные срубы из бруса: строгий контроль качества на каждом этапе, от древесины до сборки. Гарантия долговечности, тепла и уюта.
+           </div>
+          </div>
+          <div className={z.panel2}>
+           <div className={z.picPanel2}>
+            
+           </div>
+           <div className={z.titlePanel2}>
+           Качественный материал
+           </div>
+           <div className={z.textPanel2}>
+           Надежные срубы из бруса: строгий контроль качества на каждом этапе, от древесины до сборки. Гарантия долговечности, тепла и уюта.
+           </div>
+          </div>
+          <div className={z.panel3}>
+           <div className={z.picPanel3}>
+           
+           </div>
+           <div className={z.titlePanel3}>
+           Финансовая прозрачность
+           </div>
+           <div className={z.textPanel3}>
+           Надежные срубы из бруса: строгий контроль качества на каждом этапе, от древесины до сборки. Гарантия долговечности, тепла и уюта.
+           </div>
+          </div>
+          <div className={z.panel4}>
+           <div className={z.picPanel4}>
+            
+           </div>
+           <div className={z.titlePanel4}>
+           Полный пакет документов
+           </div>
+           <div className={z.textPanel4}>
+           Надежные срубы из бруса: строгий контроль качества на каждом этапе, от древесины до сборки. Гарантия долговечности, тепла и уюта.
+           </div>
+          </div>
           </div>
           </div>
         </div>
