128888afaa4aeea72413b45952d84efa390c7d92
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-13 17:52:46 +0300

    ssksss

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Achievments from './components/achievements/Achievments';
 import AboutUs from './components/abousUs/AboutUs';
 import Projects from './components/prjects/Projects'
 import z from '../src/App.css'
+import WhyUs from './components/whyUs/WhyUs';
 function App() {
   return (
     <div className="App">
       <div className={z.header}><Header/></div>
-      
       <Achievments/>
       <AboutUs/>
       <Projects/>
+      <WhyUs/>
     </div>
   );
 }
--- src/components/whyUs/WhyUs.js
+++ src/components/whyUs/WhyUs.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import z  from './whyUs.module.css'
-const WhyUS = () => {
+import z  from './WhyUs.module.css'
+const WhyUs = () => {
   return (
     <div>
-      <div className={z.whyUsMAin}></div>
+      <div className={z.whyUsMain}>
+        <div className={z.backgroundBig}>
+          <div className={z.whyUsTitle}>
+          Почему мы?
+          </div>
+          <div className={z.containerForAll}>
+          <div className={z.whyUsText}>
+           
+          </div>
+          <div className={z.whyUsPanels}>
+
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default WhyUS
+export default WhyUs
