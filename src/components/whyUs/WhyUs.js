5c068d345ef84ac61a8b9970d559a56ac6abd06b
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-13 16:37:20 +0300

    dfhsdhjfks

---
+++ src/components/whyUs/WhyUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import z  from './whyUs.module.css'
+const WhyUS = () => {
+  return (
+    <div>
+      <div className={z.whyUsMAin}></div>
+    </div>
+  )
+}
+
+export default WhyUS
