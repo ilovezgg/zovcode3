9cc19c73b60282d5eb7b4636fe6e52403c0eb789
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-27 19:04:34 +0300

    27.02

--- src/components/pages/home/header/Buttons.js
+++ src/components/pages/home/header/Buttons.js
@@ -1,59 +1,81 @@
-import React, { useState, useEffect } from 'react'
-import z from './Buttons.module.css'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import z from "./Buttons.module.css";
+import { Link } from "react-router-dom";
 const Buttons = () => {
-    const [isTransparent, setIsTransparent] = useState(false);
+  const [isTransparent, setIsTransparent] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 290) {
-          setIsTransparent(true);
-        } else {
-          setIsTransparent(false);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 290) {
+        setIsTransparent(true);
+      } else {
+        setIsTransparent(false);
+      }
+      if (window.scrollY > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div>
-       <div className={z.headerButtons}>
-       <Link
-  to="/" 
-  className={`${z.mainButton} ${isTransparent ? z.transparentButton : ''}`} // Динамические классы
->
- Главная
-</Link>
-              <Link
-  to="/we" 
-  className={`${z.aboutusButton} ${isTransparent ? z.transparentButton : ''}`} // Динамические классы
->
-  О нас
-</Link>
-<Link
-  to="/contacts" 
-  className={`${z.contactsButton} ${isTransparent ? z.transparentButton : ''}`} // Динамические классы
->
-  Контакты
-</Link>
-<Link
-  to="/catalog" 
-  className={`${z.catalogButton} ${isTransparent ? z.transparentButton : ''}`} // Динамические классы
->
-  Контакты
-</Link>
-              <button
-                className={`${z.callbackButton} ${isTransparent ? z.transparentButton : ''}`}
-              >
-                Личный кабинет
-              </button>
-              </div>
+    <div className={z.mainHeader}>
+      <div className={z.containerButtons}>
+      <div className={`${z.backgroundBlur} ${
+              isScrolled ? z.scrollyButton : ""
+            }`} ></div>
+        <div className={z.headerButtons}>
+          <Link
+            to="/"
+            className={`${z.mainButton} ${
+              isTransparent ? z.transparentButton : ""
+            }`} 
+          >
+            Главная
+          </Link>
+          <Link
+            to="/we"
+            className={`${z.aboutusButton} ${
+              isTransparent ? z.transparentButton : ""
+            }`} 
+          >
+            О нас
+          </Link>
+          <Link
+            to="/contacts"
+            className={`${z.contactsButton} ${
+              isTransparent ? z.transparentButton : ""
+            }`} 
+          >
+            Контакты
+          </Link>
+          <Link
+            to="/catalog"
+            className={`${z.catalogButton} ${
+              isTransparent ? z.transparentButton : ""
+            }`} 
+          >
+            Каталог
+          </Link>
+          <Link
+            to="/catalog"
+            className={`${z.callbackButton} ${
+              isTransparent ? z.transparentButton : ""
+            }`} 
+          >
+            Личный кабинет
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
--- src/components/pages/we/We.js
+++ src/components/pages/we/We.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Footer from '../home/footer/Footer'
 import Buttons from '../home/header/Buttons'
 import Cards  from './cards/Cards'
+import Textwe from './textWe/Textwe'
 const We = () => {
   return (
     <div>
        <Buttons />
+       <Textwe /> 
        <Cards />
       <Footer />
     </div>
